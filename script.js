3400a35601c0d285a400681af35e1382a78229ae
Your Name <bdlifehacks32@gmail.com>
2020-02-28 07:39:10 +0600

    2/28/2020

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+
+
+$(document).ready(function(){
+    
+    $('#home').click(function(){
+       $('.drop1').slideToggle()
+    })
+    
+    $('#about').click(function(){
+       $('.drop2').slideToggle()
+    })
+    
+    $('#service').click(function(){
+       $('.drop3').slideToggle()
+    })
+    
+    $('#portfolios').click(function(){
+       $('.drop4').slideToggle()
+    })
+    
+    $('#team').click(function(){
+       $('.drop5').slideToggle()
+    })
+
+})
