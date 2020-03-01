ef7471518a1a5667a56e5a9a79a1e67ea4d3680c
Your Name <bdlifehacks32@gmail.com>
2020-03-01 10:20:32 +0600

    3/1/2020

--- script.js
+++ script.js
@@ -1,27 +1,60 @@
+$(document).ready(function() {
+
+ let m= $(".drop1");
+ let n= $(".drop2");
+ let o= $(".drop3");
+ let p= $(".drop4");
+ let q= $(".drop5");
+
+  $("#home").click(function() {
+    m.slideToggle();
+    if (m.css("display","block")) {
+      n.css("display", "none");
+      o.css("display", "none");
+      p.css("display", "none");
+      q.css("display", "none");
+    }
+  });
+
+  $("#about").click(function() {
+    n.slideToggle();
+    if (n.css("display", "block")) {
+      m.css("display", "none");
+      o.css("display", "none");
+      p.css("display", "none");
+      q.css("display", "none");
+    }
+  });
+
+  $("#service").click(function() {
+    o.slideToggle();
+    if (o.css("display", "block")) {
+      n.css("display", "none");
+      m.css("display", "none");
+      p.css("display", "none");
+      q.css("display", "none");
+    }
+  });
+
+  $("#portfolios").click(function() {
+    p.slideToggle();
+    if (p.css("display", "block")) {
+      n.css("display", "none");
+      o.css("display", "none");
+      m.css("display", "none");
+      q.css("display", "none");
+    }
+  });
+
+  $("#team").click(function() {
+    q.slideToggle();
+    if (q.css("display", "block")) {
+      n.css("display", "none");
+      o.css("display", "none");
+      p.css("display", "none");
+      m.css("display", "none");
+    }
+  });
+});
 
 
-
-
-$(document).ready(function(){
-    
-    $('#home').click(function(){
-       $('.drop1').slideToggle()
-    })
-    
-    $('#about').click(function(){
-       $('.drop2').slideToggle()
-    })
-    
-    $('#service').click(function(){
-       $('.drop3').slideToggle()
-    })
-    
-    $('#portfolios').click(function(){
-       $('.drop4').slideToggle()
-    })
-    
-    $('#team').click(function(){
-       $('.drop5').slideToggle()
-    })
-
-})
